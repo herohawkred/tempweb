db96929ecaa58baf859ea8ceb63c04b2faf7b7f0
unknown <paul@bunnit.kr>
2021-07-07 16:37:03 +0900

    safari test 02

--- src/components/Ladingpage/Headsector/Headsectorelements.js
+++ src/components/Ladingpage/Headsector/Headsectorelements.js
@@ -6,7 +6,6 @@ export const Container = styled.div`
     display:flex;
     flex-direction: column;
     background-color:#F5F5F5;
-    height:100%;
     flex: 0 0 auto;
         @media (max-width: 480px) {
             width:100%;
